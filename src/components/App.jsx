4debec1f9e30f5fc61641bb340160e61d18a6d3e
Demianovq <110334010+Demianovq@users.noreply.github.com>
2023-04-08 12:33:11 +0300

    adds

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,22 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { ContactList } from './Contacts/Contacts';
 import { FormContacts } from './form/form';
 import { Filter } from './Filter/Filter';
 import { addContact } from 'redux/contactsSlice';
+import { getContacts } from 'redux/selectors';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
 
   const handleSubmit = (values, { resetForm }) => {
+    const theSameContact = contacts.filter(
+      contact => contact.text.name.toLowerCase() === values.name.toLowerCase()
+    );
+    if (theSameContact.length) {
+      alert(`${values.name} is already in contacts`);
+      return;
+    }
     dispatch(addContact(values));
     resetForm();
   };
